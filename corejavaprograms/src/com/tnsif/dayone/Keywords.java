cf1df78150621794437582c40de0d630379490ff
Vanithagiri <vanithagiri05@gmail.com>
2025-08-18 12:16:25 +0530

    Completed Dayone program

--- corejavaprograms/src/com/tnsif/dayone/Keywords.java
+++ corejavaprograms/src/com/tnsif/dayone/Keywords.java
@@ -3,7 +3,12 @@ package com.tnsif.dayone;
 public class Keywords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number = 10; 
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Non-positive number");
+        }
 
 	}
 
--- corejavaprograms/src/com/tnsif/dayone/Student.java
+++ corejavaprograms/src/com/tnsif/dayone/Student.java
@@ -1,10 +1,16 @@
 package com.tnsif.dayone;
 
 public class Student {
+    int sid;
+    String sname;
+    float avg;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        Student s1 = new Student();
 
-	}
-
+        System.out.println("Student name: " + s1.sname); // default = null
+        System.out.println("Student Id: " + s1.sid);     // default = 0
+        System.out.println("Student Average: " + s1.avg); // default = 0.0
+    }
 }
