03ca9629783753de60d172aedecaac777c1e9791
Vanithagiri <vanithagiri05@gmail.com>
2025-08-19 14:20:57 +0530

    Inheritance program completed

---
+++ corejavaprograms/src/singleinheritence_example/SingleDemo.java
@@ -0,0 +1,11 @@
+package singleinheritence_example;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+	
+	Trainer t=new Trainer(112,"vanitha","puducherry",52,"lead","Chennai/Puducherry");
+	System.out.println(t);
+	}
+
+}
