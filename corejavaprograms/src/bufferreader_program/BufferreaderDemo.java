939aec3fedea5dbb606110153a9741fc4840e1c2
Vanithagiri <vanithagiri05@gmail.com>
2025-08-19 15:47:54 +0530

    Updated buffer reader program

---
+++ corejavaprograms/src/bufferreader_program/BufferreaderDemo.java
@@ -0,0 +1,18 @@
+package bufferreader_program;
+import java.io.*;
+public class BufferreaderDemo {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enterb the name: ");
+        String name=br.readLine();
+        System.out.println("Enter the age: ");
+        int age=Integer.parseInt(br.readLine());
+        System.out.println("Enter the salary: ");
+        double salary=Double.parseDouble(br.readLine());
+        System.out.println("Name:"+name);
+        System.out.println("Age: "+age);
+        System.out.println("Salary: "+salary);
+	
+	}
+
+}
